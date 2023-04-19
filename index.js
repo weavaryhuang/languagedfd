4120d3fb189ca01c5000e4693234aeaf2b210bb3
HM <w1a8a2y@gmail.com>
2023-04-19 17:01:19 +0100

    nodejs added

---
+++ index.js
@@ -0,0 +1,69 @@
+const http = require("http");
+const fs = require("fs");
+const port = Math.floor(Math.random()*3500)+5000;
+var fileType = "html";
+
+const sendResponse = (filename, statusCode, response) =>{
+    
+    fs.readFile(`./${filename}`, (error, data) => {
+        if(error){
+            response.statusCode = 500;
+            response.setHeader("Content-Type", "text/plain");
+            response.end("Error");
+        }else{
+            response.statusCode = 200;
+            response.setHeader("Content-Type", `text/${fileType}`);
+            console.log(fileType);
+            response.end(data);
+        }
+    });
+};
+
+http.createServer((request, response) => {
+    console.log("start");
+    console.log(request.url, request.method);
+    var method = request.method;
+    var url = request.url;
+
+
+    if(method === "GET"){
+        const requestUrl = new URL(url, `http://localhost:${port}`);
+        console.log(requestUrl);
+
+        var path = requestUrl.pathname;
+        if(path === "/" || path === "/home.html" ){
+            fileType = "html";
+            sendResponse("home.html", 200, response);
+            console.log("Finish");
+        }
+        else if(path === "/japanln.html"){
+            fileType = "html";
+            sendResponse("japanln.html", 200, response);
+            console.log("Finish");
+        }
+        else if(path === "/koreanln.html"){
+            fileType = "html";
+            sendResponse("koreanln.html", 200, response);
+        }
+        else if(path === "/json/languageDb.json"){
+            fileType = "css";
+            sendResponse("/json/languageDb.json", 200, response);
+        }
+        else if(path === "/css/style.css"){
+            fileType = "css";
+            sendResponse("/css/style.css", 200, response);
+        }
+        else if(path === "/js/script.js"){
+            fileType = "css";
+            sendResponse("js/script.js", 200, response);
+        }
+        else{
+            fileType = "html";
+            sendResponse("notPage.html", 400, response);
+        }
+    } else{
+
+    }
+
+}).listen(port, ()=> {console.log("Working... " +port);});
+
