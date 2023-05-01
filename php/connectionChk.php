64a5a10dfafa6b4c4ef49acb9d096c8c4ba72ebf
HM <w1a8a2y@gmail.com>
2023-05-01 15:38:32 +0100

    trivial update

--- php/connectionChk.php
+++ php/connectionChk.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername = "database-1.cldzhnkoxt2b.eu-west-2.rds.amazonaws.com";
+$servername = "localhost";
 $username = "root";
-$password = "gnBn2&6Vc#ai67";
+$password = "AsdfgHjkl098";
 $dbname = "mydb";
 $dbport = "3306";
 
 
 try {
-$conn = new PDO("mysql:host=$servername; port=$port; dbname=$dbname;", $username, $password);
+$conn = new PDO("mysql:host=$servername; dbname=$dbname;", $username, $password);
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Connected successfully <br>";
