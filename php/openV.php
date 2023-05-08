0c91587620369f153a2dfd4b7b5da5a2b0edb374
HM <w1a8a2y@gmail.com>
2023-05-08 19:13:40 +0100

    restful setting chp12 b

--- php/openV.php
+++ php/openV.php
@@ -28,9 +28,21 @@ include "SQL/tableSql.php";
 
 
 <br>
+<script>
 
+var chk_pp = "<?php include "phprd.php"; echo $data_r;?>";
+if(chk_pp == "HEN"){
+    document.getElementById("openformR").style.display = "block";
+    document.getElementById("openformU").style.display = "none";
+}
+else{
+    document.getElementById("openformR").style.display = "none";
+    document.getElementById("openformU").style.display = "block";
+}
 
-<form method="post" action="main.php">
+</script>
+
+<form id="openformR" style="display: none;" method="post" action="main.php">
     <select name="subject[]">
         <option value="Open">Open</option>
         <option value="Update">Update</option>
@@ -40,6 +52,15 @@ include "SQL/tableSql.php";
     <input type="submit" name="submit" value="Search">
 </form>
 
+<form id="openformU" style="display: block;" method="post" action="main.php">
+    <select name="subject[]">
+        <option value="status">Status</option>
+        <option value="message">Meassage</option>
+        <option value="change">Change</option>
+        <option value="Delete">Delete</option>
+    </select>
+    <input type="submit" name="submit" value="Search">
+</form>
 <!-- <div>
     <script>var chk_p = ?php echo $permissionUser;?;</script>
     <script>var chkPermission = (chk_p=='demotb')? 1: 0;</script>
